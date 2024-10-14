1081ff9fe4d9dfca6b36aa6badfbf4a5fe7d8227
Chad1101 <68296120+draking1101@users.noreply.github.com>
2024-10-15 00:06:00 +0800

    Add more text

--- src/components/const.tsx
+++ src/components/const.tsx
@@ -6,6 +6,19 @@ export const TEXT = {
             merch: 'Merch',
             about: 'About',
             rule: 'Rules',
+        },
+        home: {
+            bannerTitle1: 'Welcome to Xinjean Gang ♡',
+            bannerTitle2: "I love playing games, drawing, and singing ♡",
+            bannerTitle3: "My simple wish is to bring joy to you during my stream time ♡",
+            vtuberSkinFirstLive: 'Virtual skin Debut',
+            rankVideoTitle: 'Valorant Extraordinary Journey',
+            episodeTitle: 'Select episode',
+            episode1: 'Ep.1 - The opponents never give an ACE😭',
+            episode2: 'Ep.2 - Reyna 1v4 Clutch Situation in rank',
+            episode3: 'Ep.3 - Losing streak in Platinum ranked games😭',
+            episode4: 'Ep.4 - Encountered a cheater in ranked again?!',
+            episode5: 'Ep.5 - Facing multiple opponents in clutch situation with Reyna again!',
         }
     },
     zh: {
@@ -15,6 +28,19 @@ export const TEXT = {
             merch: '周邊',
             about: '關於我',
             rule: '互動規則',
+        },
+        home: {
+            bannerTitle1: "歡迎加入心結幫 ♡",
+            bannerTitle2: "我喜歡玩遊戲、畫畫、唱歌 ♡",
+            bannerTitle3: "簡單的心願就是希望能在直播的時間讓你們感到快樂 ♡",
+            vtuberSkinFirstLive: 'V皮初配信',
+            rankVideoTitle: '特戰超凡之旅',
+            episodeTitle: '選擇集數',
+            episode1: '第1集 - 對手永遠不給ACE😭',
+            episode2: '第2集 - 排位蕾娜1打4殘局！',
+            episode3: '第3集 - 白金場排位連輸心態爆炸😭',
+            episode4: '第4集 - 排位又遇外掛?!照樣1發拿下MVP😎',
+            episode5: '第5集 - 蕾娜再次一打多殘局',
         }
     },
 };
